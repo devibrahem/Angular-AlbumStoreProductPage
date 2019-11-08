5268479405d659d30d42608a1fce9d3090eb66c7
devibrahem <dev.ibrahembastawi@gmail.com>
2019-11-08 21:11:24 +0200

    fixing list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import "rxjs/add/operator/map";
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
-  private _productUrl = "../assets/products.json";
+  private _productsUrl = "../assets/products.json";
   constructor(private _http: Http) {}
   getAlbum(id: number): Observable<Album> {
     return this._http
@@ -16,7 +16,7 @@ export class ProductService {
   }
   getProducts(): Observable<Product[]> {
     return this._http
-      .get(this._productUrl)
+      .get(this._productsUrl)
       .map(response => <Product[]>response.json());
   }
 }
